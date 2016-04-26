406b0ae1b7f771595a78e3a9cd493f7eddfe995d
Guillermo Gabrielli <ggf31416@gmail.com>
2016-04-26 17:26:46 -0300

    Version de Manuel

---
+++ FrontEnd/SPA/app.js
@@ -0,0 +1,29 @@
+﻿(function () {
+
+    angular.module('tsi', [
+        'ngRoute',
+        // Custom modules 
+        'tsi.employee'
+    ]);
+
+    angular.module('tsi').config(['$routeProvider','$locationProvider', configFunction]);
+
+    /*@ngInject*/
+    function configFunction($routeProvider, $locationProvider) {
+        $locationProvider.html5Mode({ enabled: true, requireBase: false}).hashPrefix('!');  
+
+        // Routes
+        $routeProvider
+			.when("/", {
+			    templateUrl: "index.html"
+			})
+            .when("/createEmployees", {
+                templateUrl: "/SPA/employees/employees.html",
+                controller: "employeesCtrl"
+            })
+            .when("/listEmployee", {
+                templateUrl: "listEmployee.html",
+                controller: "listEmployeesCtrl"
+            }).otherwise({ redirectTo: '/' });
+    }
+})();
